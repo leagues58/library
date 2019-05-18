19d233d80a8c1fffbb5f23fa1e6f73ad5662cf7f
Eric <leagues58@gmail.com>
2019-05-18 01:10:53 -0400

    Added index html page with bootstrap

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = require('path');
+
 
 var app = express();
 
 app.use(morgan('tiny'));
 
 app.get('/', function(req, res){
-    res.send('the library says hi');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 
