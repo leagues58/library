c5d1e8237f2299bd711503c3d9a05085828202cb
Eric <leagues58@gmail.com>
2019-05-27 17:34:36 -0400

    Added some more routing and new index.ejs from template

--- app.js
+++ app.js
@@ -7,15 +7,35 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      title: 'Library App'
+    }
+  );
 });
 
 app.listen(port, () => {
